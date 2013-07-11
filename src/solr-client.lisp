fbfa07e1b1d34c0b4a0215c4f003ce8f629c4fd2
Mark Watson <markw@markwatson.com>
2013-07-11 13:14:14 -0700

    added Solr client code for adding documents to index

--- src/solr-client.lisp
+++ src/solr-client.lisp
@@ -15,3 +15,46 @@
 ;;   (cdr (cadddr (cadr (do-search "British" "one"))))
 ;; retunrs result documents a a simple list of documents without
 ;; Solr meta data.
+
+   
+(defun keys-values-to-xml-string  (keys-values-list)
+ (with-output-to-string (stream)
+   (format stream "<add><doc>")
+   (dolist (kv keys-values-list)
+     (format stream "<field name=\"")
+     (format stream (car kv))
+     (format stream "\">")
+     (format stream (cdr kv))
+     (format stream "\"</field>"))
+   (format stream "</doc><doc><commit></commit></doc></add>")))
+
+;; test:
+;; (keys-values-to-xml-string '(("id" . "12345")("title" . "test title")))
+
+
+;; add a new document to index.
+;; note: one key/value must have key of "id".
+;; if document with "id" already in index then
+;; this overwrites key-values if they were defined already.
+(defun do-add (keys-values-list)
+  (drakma:http-request
+   "http://localhost:8983/solr/update"
+   :method :post
+   :content-type "application/xml"
+   :content ( keys-values-to-xml-string  keys-values-list)))
+
+;; test:
+;; (do-add '(("id" . "12345")("title" . "Fishing Season")))
+
+(defun commit-adds ()
+  (drakma:http-request
+   "http://localhost:8983/solr/update"
+   :method :post
+   :content-type "application/xml"
+   :content "<commit></commit>"))
+
+
+
+
+
+
