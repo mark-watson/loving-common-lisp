6f7b8f215389126cf597f55458c93115c25a1685
Mark Watson <markw@markwatson.com>
2013-08-07 16:39:37 -0700

    fixed macro example (variable capture)

--- src/lambda1.lisp
+++ src/lambda1.lisp
@@ -2,3 +2,6 @@
   (let ((my-func
 	 (lambda (x) (+ x 1))))
     (funcall my-func 1)))
+
+(defun testfn (a-function a-value)
+  (a-function a-value))
--- src/macro1.lisp
+++ src/macro1.lisp
@@ -1,12 +1,14 @@
 ;; first simple macro example:
 
 (defmacro double-list (a-list)
-  `(let ((ret nil))
-    (dolist (x ,a-list)
-      (setq ret (append ret (list x x))))
-    ret))
+  (let ((ret (gensym)))
+    `(let ((,ret nil))
+       (dolist (x ,a-list)
+         (setq ,ret (append ,ret (list x x))))
+       ,ret)))
 
 ;; use the macro:
 
 (defun test (x)
   (double-list x))
+
