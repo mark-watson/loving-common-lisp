4827159c1af5d5421fa5f8a16de93a156d895532
Mark Watson <markw@markwatson.com>
2025-07-25 11:19:08 -0700

    -

--- python/python_spacy_nlp_server/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='spacy_nlp_server',
-      version='0.11',
-      packages=['spacynlpserver', 'spacynlpserver.test'],
-      license='Apache 2',
-      scripts=['bin/spacynlpserver'],
-      long_description=open('README.md').read())
