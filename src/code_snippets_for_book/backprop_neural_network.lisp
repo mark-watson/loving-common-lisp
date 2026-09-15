5d3eb7328784e4c6897150dd62a4b0455445acdb
Mark Watson <markw@markwatson.com>
2026-09-15 08:21:30 -0700

    -

--- src/code_snippets_for_book/backprop_neural_network.lisp
+++ src/code_snippets_for_book/backprop_neural_network.lisp
@@ -1,74 +1,23 @@
-# Backpropagation Neural Networks {#backprop}
+;;
+ ;  Backpropagation neural network example from the chapter
+ ;  "Backpropagation Neural Networks". Only the neural network code
+ ;  from the chapter listings is included here (no plotting code).
+ ;;
 
-**Dear reader, the code in this chapter is old but I include it from a feeling of personal nostalgia. I used an early version of this simple code in the mid-1980s to prototype the C++ code that I wrote for a bomb detector my company built for the FAA. Hopefully my nostalgia is something interesting to you.**
+(defparameter *delta-rule-debug-flag* nil)
 
-Let's start with an overview of how these networks work and then fill in more detail later. Backpropagation networks are trained by applying training inputs to the network input layer, propagate values through the network to the output neurons, compare the errors (or differences) between these propagated output values and the training data output values. These output errors are backpropagated though the network and the magnitude of backpropagated errors are used to adjust the weights in the network.
+(defparameter *delta-default-input-noise-value* 0.0)
 
-The source code for the neural network example is the file **src/code_snippets_for_book/backprop_neural_network.lisp**. The screen shots below were produced by an earlier version of the program that used a **plotlib** package and this plotting code is not included in the listings here.
+(defun frandom (min max)
+  (+ min (* (random 1.0) (- max min))))
 
-We will use the following diagram to make this process more clear. There are four weights in this very simple network:
-
-- W^1,1^ is the floating point number representing the connection strength between input_neuron^1^ and output_neuron^1^
-- W^2,1^ connects input_neuron^2^ to output_neuron^1^
-- W^1,2^ connects input_neuron^1^ to output_neuron^2^
-- W^2,2^ connects input_neuron^2^ to output_neuron^2^
-
-{width: “50%"}
-![Understanding how connection weights connect neurons in adjacent layers](images/nn_backprop2d.png)
-
-Before any training the weight values are all small random numbers.
-
-Consider a training data element where the input neurons have values [0.1, 0.9] and the desired output neuron values are [0.9 and 0.1], that is flipping the input values. If the propagated output values for the current weights are [0.85, 0.5] then the value of the first output neuron has a small error abs(0.85 - 0.9) which is 0.05. However the propagated error of the second output neuron is high: abs(0.5 - 0.1) which is 0.4. Informally we see that the weights feeding input output neuron 1 (W^1,1^ and W^2,1^) don't need to be changed much but the neuron that feeding input neuron 2 (W^1,2^ and W^2,2^) needs modification (the value of W^2,2^ is too large).
-
-Of course, we would never try to manually train a network like this but it is important to have at least an informal understanding of how weights connect the flow of value (we will call this activation value later) between neurons.
-
-In this neural network see in the first figure we have four weights connecting the input and output neurons. Think of these four weights forming a four-dimensional space where the range in each dimension is constrained to small positive and negative floating point values. At any point in this "weight space", the numeric values of the weights defines a model that maps the inputs to the outputs. The error seen at the output neurons is accumulated for each training example (applied to the input neurons). The training process is finding a point in this four-dimensional space that has low errors summed across the training data. We will use gradient descent to start with a random point in the four-dimensional space (i.e., an initial random set of weights) and move the point towards a local minimum that represents the weights in a model that is (hopefully) "good enough" at representing the training data.
-
-This process is simple enough but there are a few practical considerations:
-
-- Sometimes the accumulated error at a local minimum is too large even after many training cycles and it is best to just restart the training process with new random weights.
-- If we don't have enough training data then the network may have enough memory capacity to memorize the training examples. This is not what we want: we want a model with just enough memory capacity (as represented by the number of weights) to form a generalized predictive model, but not so specific that it just memorizes the training examples. The solution is to start with small networks (few hidden neurons) and increase the number of neurons until the training data can be learned. In general, having a lot of training data is good and it is also good to use as small a network as possible.
-
-In practice using backpropagation networks is an iterative process of experimenting with the size of a network.
-
-An earlier version of the example program (in the file *backprop_neural_network.lisp*) used the plotting library developed earlier to visualize neuron activation and connecting weight values while the network trains; the screen shots below show that visualization.
-
-The following three screen shots illustrate the process of starting with random weights, getting random outputs during initial training, and as delta weights are used to adjust the weights in a network, then the training examples are learned:
-
-{width: "40%"}
-![At the start of the training run with random weights and large delta weights](images/output_plot_000000000000.png)
-
-In the last figure the initial weights are random so we get random mid-range values at the output neurons.
-
-{width: "40%"}
-![The trained weights start to produce non-random output](images/output_plot_000000000400.png)
-
-As we start to train the network, adjusting the weights, we start to see variation in the output neurons as a function of what the inputs are.
-
-{width: "40%"}
-![After training many cycles the training examples are learned, with only small output errors](images/output_plot_000000002400.png)
-
-In the last figure the network is trained sufficiently well to map inputs [0, 0, 0, 1] to output values that are approximately [0.8, 0.2, 0.2, 0.3] which is close to the expected value [1, 0, 0, 0].
-
-The activation values of individual neurons are limited to the range [0, 1] by first calculating their values based on the sum activation values of neurons in the previous layer times the values of the connecting weights and then using the **Sigmoid** function to map the sums to the desired range. The Sigmoid function and the derivative of the **Sigmoid** function (**dSigmoid**) look like:
-
-{width: "60%"}
-![Sigmoid and Derivative of the Sigmid Functions](images/nn_sigmoid.png)
-
-Here are the definitions of these functions:
-
-```lisp
 (defun Sigmoid (x)
   (/ 1.0 (+ 1.0 (exp (- x)))))
 
 (defun dSigmoid (x)
   (let ((temp (Sigmoid x)))
     (* temp (- 1.0 temp))))
-```
 
-The function **NewDeltaNetwork** creates a new neural network object. This code allocates storage for input, hidden, output layers (I sometimes refer to neuron layers as "slabs"), and the connection weights. Connection weights are initialized to small random values.
-
-```lisp
 ; (NewDeltaNetwork sizeList)
 ;       Args:   sizeList = list of sizes of slabs. This also defines
 ;                          the number of slabs in the network.
@@ -153,12 +102,7 @@ The function **NewDeltaNetwork** creates a new neural network object. This code 
      w-list)
     (list numLayers sizeList a-list s-list w-list dw-list
           d-list old-dw-list alpha beta))
-```
 
-In the following listing the function **DeltaLearn** processes one pass through all of the training data. Function **DeltaLearn** is called repeatedly until the return value is below a desired error threshold. The main loop over each training example is implemented in lines 177-272. Inside this outer loop there are two phases of training for each training example: a forward pass propagating activation from the input neurons to the output neurons via any hidden layers (lines 191-209) and then the backpropagation of output errors to accumulate the delta weights (lines 210-272). After all of the training examples have been processed, the weights are updated (lines 274-294):
-
-
-```lisp
 ;;
  ;  Utility function for training a delta rule neural network.
  ;  The first argument is a network definition (as returned from
@@ -350,11 +294,7 @@ In the following listing the function **DeltaLearn** processes one pass through 
       (setq iDimension jDimension))
 
     (/ outputError jDimension)))
-```
 
-The function **DeltaRecall** in the next listing can be used with a trained network to calculate outputs for new input values:
-
-```lisp
 ;;
  ;  Utility for using a trained neural network in the recall mode.
  ;  The first argument to this function is a network definition (as
@@ -399,11 +339,7 @@ The function **DeltaRecall** in the next listing can be used with a trained netw
       (dotimes (j jDimension)
         (setq returnList (append returnList (list (aref jActivationVector j)))))
       returnList))
-```
 
-We saw three output plots earlier that were produced during a training run using the following code:
-
-```lisp
 (defun test3 (&optional (restart 'yes) &aux RMSerror (temp nil)) ; three layer network
   (if
     (equal restart 'yes)
@@ -425,10 +361,3 @@ We saw three output plots earlier that were produced during a training run using
         (princ " RMS error = ")
         (princ RMSerror)
         (terpri)))))
-```
-
-Here the function **test3** defines training data for a very small test network for a moderately difficult function to learn: to rotate the values in the input neurons to the right, wrapping around to the first neuron. The start of the main loop calls the training function 3000 times, printing the RMS error every 50 times through the main loop.
-
-Backpropagation networks have been used sucessfully in production for about 25 years. In the next chapter we will look at a less practical type of network, Hopfield networks, that are still interesting because the in some sense Hopfield networks model how our brains work. In the final chapter we will look at deep learning neural networks.
-
-
